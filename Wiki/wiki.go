2fb0121bc90fe3c3ba70f226006ed3db40319068
fenzhu <yheverlove27@outlook.com>
2024-08-16 10:58:08 +0800

    wiki, view edit save

--- Wiki/wiki.go
+++ Wiki/wiki.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
 func main() {
 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page")}
@@ -23,11 +26,68 @@ func main() {
 	fmt.Println(string(p2.Body))
 
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+
+	p, err := loadPage(title)
+	if err != nil {
+		log.Printf("page %s not exsit, redirec to edit", title)
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	// t, err := template.ParseFiles(tmpl + ".html")
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// }
+
+	// err = t.Execute(w, p)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// }
 }
 
 type Page struct {
