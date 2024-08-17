75666e19804a2c664e1ed0a7a692ccc540bc8334
fenzhu <yheverlove27@outlook.com>
2024-08-17 12:58:52 +0800

    tour, error

--- Tour/method.go
+++ Tour/method.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"math"
+	"os"
 	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/tour/reader"
 )
 
 type Point struct {
@@ -44,6 +51,21 @@ func method() {
 	for name, ip := range hosts {
 		fmt.Printf("%v: %v\n", name, &ip)
 	}
+
+	if err := RunError(); err != nil {
+		//priority, error() > string()
+		fmt.Println(err)
+	}
+
+	fmt.Println(sqrtChecked(2))
+	fmt.Println(sqrtChecked(-2))
+
+	read()
+	reader.Validate(&MyReader{})
+
+	s13 := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r13 := rot13Reader{s13}
+	io.Copy(os.Stdout, &r13)
 
 	fmt.Printf("\n")
 }
@@ -93,4 +115,91 @@ func (ip *IPAddr) String() string {
 	}
 	fmt.Println(arr)
 	return str[:len(str)-1]
+}
+
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
+
+func (e *MyError) String() string {
+	return fmt.Sprintf("string at %v, %s", e.When, e.What)
+}
+
+func RunError() *MyError {
+	return &MyError{time.Now(), "it didn't work"}
+}
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	// convert to float64, otherwise call Error() infinitely
+	// fmt.Println(float64(e))
+	return fmt.Sprintf("cannot Sqrt negative number: %f", e)
+}
+
+func sqrtChecked(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	} else {
+		z := 1.0
+		delta := 0.001
+		//newton's method
+		for math.Abs(z*z-x) > delta {
+			z -= (z*z - x) / (2 * z)
+			fmt.Printf("z: %f ", z)
+		}
+		fmt.Printf("\n")
+		return z, nil
+	}
+}
+
+func read() {
+	r := strings.NewReader("Hello, Reader!")
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v, err = %v, b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
+}
+
+type MyReader struct{}
+
+func (r *MyReader) Read(b []byte) (int, error) {
+	for i, _ := range b {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r *rot13Reader) Read(b []byte) (int, error) {
+	len, ok := r.r.Read(b)
+	if ok != io.EOF {
+		for i := range b {
+			b[i] = rot13(b[i])
+		}
+	}
+	return len, ok
+}
+
+func rot13(x byte) byte {
+	input := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+	output := []byte("NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm")
+	match := bytes.IndexByte(input, x)
+	if match == -1 {
+		return x
+	}
+	return output[match]
 }
