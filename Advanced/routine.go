3386467fe13ac413718b891189fb68f97a4dd18b
fenzhu <yheverlove27@outlook.com>
2025-07-07 00:54:05 +0800

    golang advanced

--- Advanced/routine.go
+++ Advanced/routine.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"runtime"
 	"sync"
 )
 
 func main() {
 	PrintWorker()
+	SliceMemory()
+	MemoryLeak()
 }
 
 func PrintWorker() {
@@ -45,4 +49,59 @@ func PrintWorker() {
 
 	wg.Wait() // 等待所有协程执行完毕
 	fmt.Println("done")
+}
+
+func SliceMemory() {
+	var a = make([]int, 8)
+	fmt.Printf("len %v cap %v\n", len(a), cap(a))
+	a = append(a, 1)
+	fmt.Printf("len %v cap %v\n", len(a), cap(a))
+	a = make([]int, 1024)
+	fmt.Printf("len %v cap %v\n", len(a), cap(a))
+	a = append(a, 1)
+	fmt.Printf("len %v cap %v\n", len(a), cap(a))
+
+}
+
+func MemoryLeak() {
+	fmt.Printf("Goroutines at start: %d\n", runtime.NumGoroutine())
+	ch := make(chan int, 1)
+
+	// 1. go routine
+	// 该go routine阻塞在一个不会有数据写入的channel上
+	// 泄漏：go routine的栈内存、其引用的堆内存不会被回收
+	// 思路：确保每个go routine都有明确的退出路径
+	// 最佳实践： 使用context管理go routine的生命周期
+	go func(ctx context.Context) {
+		select {
+		case val := <-ch:
+			fmt.Println(val)
+		case <-ctx.Done():
+			fmt.Println("context done")
+		}
+	}(context.Background())
+	fmt.Printf("Goroutines at end: %d\n", runtime.NumGoroutine())
+
+	// 2. 切片
+	big := make([]byte, 1024*1024)
+	small := big[:10]
+	// 只引用了big的10个字节，但是它指向原始的底层数组，所以big的1MB内存不会被回收
+	fmt.Println(small)
+
+	// 3. time.Ticker
+
+	// 4. 全局变量不断添加数据，不清理
+
+	// 5. 闭包
+	large := make([]byte, 1024*1024)
+	closure := func() {
+		_ = large
+	}
+	// closure存活，那么闭包引用的1MB内存就不会被释放
+	// 闭包不要无意中捕获大的、不必要的变量，如果只是需要变量中的一部分数据，可以将数据作为参数传递
+	_ = closure
+
+	// 6. 排查内存泄漏
+	// 使用/pprof/heap
+	// 使用/pprof/goroutine
 }
